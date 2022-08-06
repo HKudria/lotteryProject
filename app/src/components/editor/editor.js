fe336e088d7401683c70508df654837fde0ef91b
Herman Kudria <h.kudrya@hotmail.com>
2022-08-06 15:19:30 +0200

    rewrite app to react with axios library, add endpoint deletePage

---
+++ app/src/components/editor/editor.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React, {Component} from "react";
+
+export default class Editor extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageList: [],
+            newPageName: ""
+        }
+        this.createNewPage = this.createNewPage.bind(this)
+    }
+
+    componentDidMount() {
+        this.loadPageList();
+    }
+
+    loadPageList(){
+        axios
+            .get("./api")
+            .then(res => this.setState({pageList: res.data}))
+    }
+
+    createNewPage() {
+        axios
+            .post("./api/createNewPage.php", {"name": this.state.newPageName})
+            .then(this.loadPageList())
+            .catch(()=>{alert('page is exist!')});
+    }
+
+    deletePage(page){
+        axios
+            .post("./api/deletePage.php", {"name": page})
+            .then(this.loadPageList())
+            .catch(()=>{alert('page isn\'t exist!')});
+    }
+
+    render() {
+        const {pageList} = this.state;
+        const pages= pageList.map((page,key) => {
+            return (
+                <h1 key={key}>{page}
+                    <a href="#"
+                    onClick={() => this.deletePage(page)}>(x)</a>
+                </h1>
+            )
+        })
+        return (
+            <>
+                <input
+                    onChange={(e)=> {this.setState({newPageName: e.target.value})}}
+                    type="text"/>
+                <button onClick={this.createNewPage}>Create page</button>
+                {pages}
+            </>
+        );
+    }
+
+}
--- app/src/main.js
+++ app/src/main.js
@@ -1,33 +1,34 @@
-import $ from 'jquery';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import Editor from "./components/editor";
 
-
-
-//$("h1").fadeOut();
+const root = createRoot(document.getElementById('root'));
+root.render(<Editor />);
 
 //parse array from php api using foreach in jquery
 //show all html file from site catalog
-function getPageList(){
-   $("h1").remove();
-    $.get("./api", data=>{
-        data.forEach(file => {
-            $("body").append(`<h1>${file}</h1>`)
-        })
-    },"JSON");
-}
+// function getPageList(){
+//    $("h1").remove();
+//     $.get("./api", data=>{
+//         data.forEach(file => {
+//             $("body").append(`<h1>${file}</h1>`)
+//         })
+//     },"JSON");
+// }
 
 //call function to show all html pages
-getPageList();
+// getPageList();
 
 
 //search button on page and processed click
 //when button was pushed down we send request to php page and check if page is exist else create new page
-$("button").click(()=>{
-    $.post("./api/createNewPage.php", {
-        "name" : $("input").val()
-    }, ()=>{
-        getPageList();
-    })
-        .fail(()=> {
-            alert("The page already exists!");
-        })
-});
+// $("button").click(()=>{
+//     $.post("./api/createNewPage.php", {
+//         "name" : $("input").val()
+//     }, ()=>{
+//         getPageList();
+//     })
+//         .fail(()=> {
+//             alert("The page already exists!");
+//         })
+// });

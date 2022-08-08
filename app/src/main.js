68f7f3dc3dda9c68a7fd68afda710ae25d081e07
Herman Kudria <h.kudrya@hotmail.com>
2022-08-08 15:40:13 +0200

    deploy version

--- app/src/main.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-import Editor from "./components/editor";
-
-const root = createRoot(document.getElementById('root'));
-root.render(<Editor />);

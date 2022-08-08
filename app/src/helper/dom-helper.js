831aa32c1580cfceb1d895024720b45f3229e05f
Herman Kudria <h.kudrya@hotmail.com>
2022-08-08 12:10:45 +0200

    change image

--- app/src/helper/dom-helper.js
+++ app/src/helper/dom-helper.js
@@ -25,6 +25,19 @@ export default class DOMHelper {
         return dom
     }
 
+    static wrapImages(dom) {
+        dom.body.querySelectorAll('img').forEach((img, index) => {
+            img.setAttribute('editableimgid', index);
+        })
+        return dom
+    }
+
+    static unwrapImages(dom) {
+        dom.body.querySelectorAll('[editableimgid]').forEach((img, index) => {
+            img.removeAttribute('editableimgid');
+        })
+    }
+
     static unwrapTextNodes(dom){
         dom.body.querySelectorAll("text-editor").forEach(element => {
             element.parentNode.replaceChild(element.firstChild, element);
--- app/src/main.js
+++ app/src/main.js
@@ -4,31 +4,3 @@ import Editor from "./components/editor";
 
 const root = createRoot(document.getElementById('root'));
 root.render(<Editor />);
-
-//parse array from php api using foreach in jquery
-//show all html file from site catalog
-// function getPageList(){
-//    $("h1").remove();
-//     $.get("./api", data=>{
-//         data.forEach(file => {
-//             $("body").append(`<h1>${file}</h1>`)
-//         })
-//     },"JSON");
-// }
-
-//call function to show all html pages
-// getPageList();
-
-
-//search button on page and processed click
-//when button was pushed down we send request to php page and check if page is exist else create new page
-// $("button").click(()=>{
-//     $.post("./api/createNewPage.php", {
-//         "name" : $("input").val()
-//     }, ()=>{
-//         getPageList();
-//     })
-//         .fail(()=> {
-//             alert("The page already exists!");
-//         })
-// });

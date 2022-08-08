12aacd11b75b8882451eff8b4d8a3ef835a8a753
Herman Kudria <h.kudrya@hotmail.com>
2022-08-08 15:30:43 +0200

    prepare deploy veriosn

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 const gulp = require("gulp");
 const webpack = require("webpack-stream");
-const sass = require('gulp-sass')(require('sass'));
-const dist = "./admin/";
+const sass = require("gulp-sass");
+const autoprefixer = require("autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const postcss = require("gulp-postcss");
+
+const dist = "/Applications/MAMP/htdocs/react_admin/admin";
+const prod = "./build/";
 
 gulp.task("copy-html", () => {
     return gulp.src("./app/src/index.html")
-        .pipe(gulp.dest(dist))
+        .pipe(gulp.dest(dist));
 });
 
 gulp.task("build-js", () => {
     return gulp.src("./app/src/main.js")
         .pipe(webpack({
-            mode:'development',
+            mode: 'development',
             output: {
                 filename: 'script.js'
             },
@@ -37,7 +42,7 @@ gulp.task("build-js", () => {
                 ]
             }
         }))
-        .pipe(gulp.dest(dist))
+        .pipe(gulp.dest(dist));
 });
 
 gulp.task("build-sass", () => {
@@ -48,26 +53,70 @@ gulp.task("build-sass", () => {
 
 gulp.task("copy-api", () => {
     gulp.src("./app/api/**/.*")
-        .pipe(gulp.dest(dist+"/api"));
+        .pipe(gulp.dest(dist + "/api"));
 
     return gulp.src("./app/api/**/*.*")
-        .pipe(gulp.dest(dist+"/api"));
+        .pipe(gulp.dest(dist + "/api"));
 });
 
 gulp.task("copy-assets", () => {
     return gulp.src("./app/assets/**/*.*")
-        .pipe(gulp.dest(dist+"/assets"));
+        .pipe(gulp.dest(dist + "/assets"));
 });
 
-gulp.task("watch", ()=>{
+gulp.task("watch", () => {
     gulp.watch("./app/src/index.html", gulp.parallel("copy-html"));
     gulp.watch("./app/assets/**/*.*", gulp.parallel("copy-assets"));
     gulp.watch("./app/api/**/*.*", gulp.parallel("copy-api"));
     gulp.watch("./app/scss/**/*.scss", gulp.parallel("build-sass"));
     gulp.watch("./app/src/**/*.js", gulp.parallel("build-js"));
-})
-
+});
 
 gulp.task("build", gulp.parallel("copy-html", "copy-assets", "copy-api", "build-sass", "build-js"));
+
+gulp.task("prod", () => {
+    gulp.src("./app/src/index.html")
+        .pipe(gulp.dest(prod));
+    gulp.src("./app/api/**/.*")
+        .pipe(gulp.dest(prod + "/api"));
+    gulp.src("./app/api/**/*.*")
+        .pipe(gulp.dest(prod + "/api"));
+    gulp.src("./app/assets/**/*.*")
+        .pipe(gulp.dest(prod + "/assets"));
+
+    gulp.src("./app/src/main.js")
+        .pipe(webpack({
+            mode: 'production',
+            output: {
+                filename: 'script.js'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: false,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }],
+                                    "@babel/react"]
+                            }
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(gulp.dest(prod));
+
+    return gulp.src("./app/scss/style.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(prod));
+});
 
 gulp.task("default", gulp.parallel("watch", "build"));

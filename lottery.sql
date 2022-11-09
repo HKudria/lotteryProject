029bd82bd778f8e5b52b43a545025477086e2483
Herman Kudria <h.kudrya@hotmail.com>
2022-11-09 12:33:35 +0100

    Update lottery.sql

--- lottery.sql
+++ lottery.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 10, 2022 at 09:41 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: localhost:3306
+-- Generation Time: Nov 09, 2022 at 11:24 AM
+-- Server version: 10.5.16-MariaDB
+-- PHP Version: 7.3.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -17,11 +18,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `lottery`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `lottery`
@@ -37,6 +33,11 @@ CREATE TABLE `lottery` (
   `token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `lottery`
+--
+
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +52,10 @@ CREATE TABLE `lottery_present` (
   `token_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `lottery_present`
+--
+
 -- --------------------------------------------------------
 
 --
@@ -60,8 +65,10 @@ CREATE TABLE `lottery_present` (
 CREATE TABLE `prize_log` (
   `id` int(11) NOT NULL,
   `token` varchar(40) NOT NULL,
-  `box_number` int(11) NOT NULL
+  `box_number` int(11) NOT NULL,
+  `user_token` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 -- --------------------------------------------------------
 
@@ -78,9 +85,6 @@ CREATE TABLE `user_token` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `lottery`
@@ -114,25 +118,25 @@ ALTER TABLE `user_token`
 -- AUTO_INCREMENT for table `lottery`
 --
 ALTER TABLE `lottery`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `lottery_present`
 --
 ALTER TABLE `lottery_present`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `prize_log`
 --
 ALTER TABLE `prize_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=211;
 
 --
 -- AUTO_INCREMENT for table `user_token`
 --
 ALTER TABLE `user_token`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=280;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
